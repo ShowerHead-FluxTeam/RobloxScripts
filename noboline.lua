379a2ae28b02c4a4c7552f89d079ee625d014d91
ShowerHead-FluxTeam <63327606+ShowerHead-FluxTeam@users.noreply.github.com>
2021-09-04 04:15:30 -0400

    Update noboline.lua

--- noboline.lua
+++ noboline.lua
@@ -1,3 +1,4 @@
+-- Noboline 1.4.2 (1c447d04-10ac-4fae-a010-8905d7fafb61)
 -- Script by dogix
 -- Kavo UI Library by xHeptc
-loadstring(game:HttpGet'https://dogix.wtf/scripts/noboline/f3d52145-4816-407d-aac1-cad87fd19b3b')()
+loadstring(game:HttpGet'https://dogix.wtf/scripts/noboline/1c447d04-10ac-4fae-a010-8905d7fafb61')()
